a33b6f16192a085a81e924fbeb5be24c7b5916f8
Ovidiu Sabou <ovidiu.sabou@elan-solutions.eu>
2016-02-01 15:22:22 +0200

    work on get movies from lists

--- functions.py
+++ functions.py
@@ -3,16 +3,12 @@ import urllib.request
 import urllib.response
 import json
 
+from model.FilmList import FilmList
+
 
 def read_film_list(page_index):
     website = 'http://www.tasteofcinema.com/category/lists/film-lists/page/' + str(page_index) + '/'
-    headers = {'Accept-Charset': 'utf-8'}
-
-    request = urllib.request.Request(website, headers=headers)
-
-    response = urllib.request.urlopen(request)
-    html = response.read()
-    response.close()
+    html = get_url_content(website)
 
     print("Parsing: ", website)
     from toc_parser.page_parser import TocPageParser
@@ -22,6 +18,24 @@ def read_film_list(page_index):
     return parser.get_film_list()
 
 
+def get_url_content(website):
+    headers = {'Accept-Charset': 'utf-8'}
+    request = urllib.request.Request(website, headers=headers)
+    response = urllib.request.urlopen(request)
+    html = response.read()
+    response.close()
+    return html
+
+
 def write_lists_to_json(film_list):
     with codecs.open("output/film_list.json", "w", 'utf-8') as output_file:
         json.dump(film_list, output_file, ensure_ascii=False, indent=4)
+
+
+def read_lists_from_json():
+    with codecs.open("output/film_list.json", "r", 'utf-8') as input_file:
+        json_load = json.load(input_file)
+    return json_load
+
+def get_movies(list_url):
+    html = get_url_content(list_url)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import functions
 from model.FilmList import FilmList
 
-all_lists = list()
-for index in range(1, 77):
-    film_list = functions.read_film_list(index)
-    for key in film_list:
-        filmList = FilmList(key, film_list[key])
-        all_lists.append(filmList.__dict__)
+# all_lists = list()
+# for index in range(1, 77):
+#     film_list = functions.read_film_list(index)
+#     for key in film_list:
+#         filmList = FilmList(key, film_list[key])
+#         all_lists.append(filmList.__dict__)
 
-functions.write_lists_to_json(all_lists)
+# functions.write_lists_to_json(all_lists)
+LISTS_FROM_JSON = functions.read_lists_from_json()
+print(LISTS_FROM_JSON)
