8c30bd52211c461363d1c37048db28e2bfedb95c
coco <coco.marinaru@gmail.com>
2016-02-08 00:50:44 +0200

    getting imdb link

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
 import codecs
+import json
 import urllib.request
 import urllib.response
-import json
+import urllib.parse
 
-from model.FilmList import FilmList
+from lxml import html
+
+output_dir = 'output'
 
 
 def read_film_list(page_index):
@@ -11,15 +14,16 @@ def read_film_list(page_index):
     html = get_url_content(website)
 
     print("Parsing: ", website)
-    from toc_parser.page_parser import TocPageParser
-    parser = TocPageParser()
+    from toc_parser.list_parser import TocListParser
+    parser = TocListParser()
     parser.feed(html.decode('utf-8'))
     parser.close()
     return parser.get_film_list()
 
 
 def get_url_content(website):
-    headers = {'Accept-Charset': 'utf-8'}
+    headers = {'Accept-Charset': 'utf-8',
+               'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36'}
     request = urllib.request.Request(website, headers=headers)
     response = urllib.request.urlopen(request)
     html = response.read()
@@ -27,17 +31,42 @@ def get_url_content(website):
     return html
 
 
-def write_lists_to_json(film_list):
-    with codecs.open("output/film_list.json", "w", 'utf-8') as output_file:
+def write_lists_to_json(film_list, file_name):
+    file_path = output_dir + '/' + file_name
+    with codecs.open(file_path, "w", 'utf-8') as output_file:
         json.dump(film_list, output_file, ensure_ascii=False, indent=4)
 
 
-def read_lists_from_json():
-    with codecs.open("output/film_list.json", "r", 'utf-8') as input_file:
+def read_lists_from_json(file_name):
+    file_path = output_dir + '/' + file_name
+    with codecs.open(file_path, "r", 'utf-8') as input_file:
         json_load = json.load(input_file)
     return json_load
 
 
+def remove_bad_results(movies):
+    index = movies.index("Subscribe via ")
+    return movies[:index]
+
+
 def get_movies(list_url):
-    html = get_url_content(list_url)
-    print(html)
+    content = get_url_content(list_url)
+    tree = html.fromstring(content)
+    movies = tree.xpath('//span[@style="font-family: Helvetica; font-size: 20px;"]/text()')
+    movies = remove_bad_results(movies)
+    print("Parsing... " + list_url)
+    other_pages = tree.xpath('//p[@class="pages"]/span/a/@href')
+    return movies, other_pages
+
+
+def get_imdb_link(movie):
+    imdb_prefix = 'http://www.imdb'
+    google_search_page = 'http://www.google.ro/search?q='
+    url_encoded_movie = urllib.parse.quote_plus(movie + " imdb")
+    url = google_search_page + url_encoded_movie
+    content = get_url_content(url)
+    tree = html.fromstring(content)
+    text = tree.xpath('.//ol[@id="rso"]/div[1]/div[1]//cite/text()')
+    imdb_link = imdb_prefix + text[1]
+    print("Search for:", movie, "  link:", imdb_link)
+    return imdb_link
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import functions
-from model.FilmList import FilmList
+import re
 
+lists_file = 'film_list.json'
+titles_file = "film_list_with_movies.json"
 # all_lists = list()
 # for index in range(1, 77):
 #     film_list = functions.read_film_list(index)
@@ -9,8 +11,95 @@ from model.FilmList import FilmList
 #         all_lists.append(filmList.__dict__)
 
 # functions.write_lists_to_json(all_lists)
-LISTS_FROM_JSON = functions.read_lists_from_json()
-print(LISTS_FROM_JSON)
+# LISTS_FROM_JSON = functions.read_lists_from_json(lists_file)
+#
+# print("Parsing ", len(LISTS_FROM_JSON))
+# for movie_list in LISTS_FROM_JSON:
+#     movies, pages = functions.get_movies(movie_list.get('link'))
+#     for page in pages:
+#         next_movies, next_pages = functions.get_movies(page)
+#         movies.extend(next_movies)
+#     movie_list['movies'] = movies
+#
+# functions.write_lists_to_json(LISTS_FROM_JSON, titles_file)
 
-firstElement = LISTS_FROM_JSON[0]
-functions.get_movies(firstElement.get('link'))
+movie_lists = functions.read_lists_from_json(titles_file)
+
+failed_parsing = list()
+all_movies = list()
+for movie_list in movie_lists:
+    if len(movie_list['movies']) == 0:
+        failed_parsing.append(movie_list['link'])
+    else:
+        all_movies.extend(movie_list['movies'])
+
+process_movies = set()
+
+for movie in all_movies:
+    if movie[len(movie) - 1] == '(':
+        movie = movie[:len(movie) - 2]
+    movie = movie.strip().lower()
+    matched = re.findall(r'^\d+?[.|)](.*)', movie)
+    if len(matched) == 0:
+        process_movies.add(movie)
+    else:
+        process_movies.add(matched[0].strip())
+
+sorted_movies = list()
+sorted_movies.extend(process_movies)
+sorted_movies.sort()
+
+print("Parsing failed for :", len(failed_parsing), " lists")
+
+movie_obj_list = list()
+for movie in sorted_movies:
+    movie_info = re.findall(r'(.+?)\(.*(\d{4}).*\)', movie)
+
+    movie_obj = None
+    if len(movie_info) == 0:
+        movie_obj = {'title': movie, 'year': 'N/A'}
+    else:
+        movie_obj = {'title': movie_info[0][0].strip(), 'year': movie_info[0][1]}
+
+    movie_obj_list.append(movie_obj)
+
+print("Movies to search for :", len(movie_obj_list))
+
+movie_obj_list_no_duplicates = list()
+
+previous_movie = {'title': 'test', 'year': 'test'}
+for movie_obj in movie_obj_list:
+    equal = False
+    year = previous_movie['year']
+    if previous_movie['title'] == movie_obj['title']:
+        if previous_movie['year'] == movie_obj['year']:
+            equal = True
+            year = previous_movie['year']
+        elif previous_movie['year'] == 'N/A':
+            equal = True
+            year = movie_obj['year']
+        elif movie_obj['year'] == 'N/A':
+            equal = True
+            year = previous_movie['year']
+        else:
+            equal = False
+    else:
+        equal = False
+
+    if equal:
+        index = len(movie_obj_list_no_duplicates) - 1
+        movie_obj_list_no_duplicates[index]['year'] = year
+    else:
+        movie_obj_list_no_duplicates.append(movie_obj)
+        previous_movie = movie_obj
+
+print("Movies to search for: ", len(movie_obj_list_no_duplicates))
+
+for movie_obj in movie_obj_list_no_duplicates:
+
+    year = movie_obj['year']
+    if year == 'N/A':
+        year = ''
+    search_query = movie_obj['title'] + ' ' + year
+    imdblink = functions.get_imdb_link(search_query)
+    movie_obj['imdb_link'] = imdblink
